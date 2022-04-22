8c3148d74aadf753d8b2fc632fc7aa3e32cae713
FellowNewLearner <100922159+FellowNewLearner@users.noreply.github.com>
2022-04-22 17:49:10 +0300

    Add files via upload

---
+++ Diziler/Sayi_Girilen.java
@@ -0,0 +1,33 @@
+package Diziler;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Sayi_Girilen {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int number = 0;
+
+        System.out.print("Dizi Büyüklüðü : ");
+        int n = input.nextInt();
+        int[] matrix = new int[n];
+
+        for(int i = 0; i < matrix.length; i++){
+            System.out.print((i + 1) + ". Sayýyý Giriniz : ");
+            matrix[i] = input.nextInt();
+            if(i == (matrix.length - 1)){
+                Arrays.sort(matrix);
+                System.out.println("Sýralama = " + Arrays.toString(matrix));
+                break;
+            }
+        }
+
+
+
+
+      }
+    }
+
+
