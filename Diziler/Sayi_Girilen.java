d5974b745ec59c0f0eedc6ec06a0b62a3ed71ab8
FellowNewLearner <100922159+FellowNewLearner@users.noreply.github.com>
2022-04-22 17:53:16 +0300

    Update Sayi_Girilen.java

--- Diziler/Sayi_Girilen.java
+++ Diziler/Sayi_Girilen.java
@@ -7,26 +7,18 @@ public class Sayi_Girilen {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);  
-
-        System.out.print("Dizi Büyüklüðü : ");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Dizi Büyüklüğü : ");
         int n = input.nextInt();
         int[] matrix = new int[n];
 
         for(int i = 0; i < matrix.length; i++){
-            System.out.print((i + 1) + ". Sayýyý Giriniz : ");
+            System.out.print((i + 1) + ". Sayıyı Giriniz : ");
             matrix[i] = input.nextInt();
-            if(i == (matrix.length - 1)){
-                Arrays.sort(matrix);
-                System.out.println("Sýralama = " + Arrays.toString(matrix));
-                break;
-            }
         }
-
-
-
-
-      }
+        Arrays.sort(matrix);
+        System.out.println("Sıralama = " + Arrays.toString(matrix));
     }
 
+}
 
